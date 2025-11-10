7f47a51db7775f5f34d2d2d4f7b33056bf74d79d
idanrachmany <idanrachmany@gmail.com>
2025-11-10 18:40:01 +0200

    try to add domain for localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // מאפשר גישה מחוץ לlocalhost
+    allowedHosts: ['popcornai'], // 👈 מוסיף את הדומיין שלך לרשימת המורשים
+  },
+  preview: {
+    allowedHosts: ['popcornai'], // 👈 כדי שיעבוד גם במצב preview
+  },
 })
